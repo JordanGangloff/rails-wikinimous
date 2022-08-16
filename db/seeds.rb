6140a69ce77c0d7c527dbe6c4a9fc2bf45ad19df
Jordan Gangloff <jordangangloff@icloud.com>
2022-08-16 16:50:07 +0200

    not beautiful but working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence(word_count: 4),
+    content: Faker::Lorem.sentence(word_count: 30)
+  )
+  article.save!
+end
